a8ad4b3f4ee1a04d8f27e22f174cba96f7d11e0a
Helene Buffet <hebuffet@gmail.com>
2022-11-08 16:06:27 -0500

    seed ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+# creating 10 fake articles
+10.times do
+  Article.create(title: Faker::GreekPhilosophers.quote, content: Faker::Lorem.paragraph)
+end
